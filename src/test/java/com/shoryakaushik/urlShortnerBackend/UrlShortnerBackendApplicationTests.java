24919926296c97d8b6c7439f72a45c11825b7139
Shorya Kaushik <shorya117@gmail.com>
2021-05-14 17:21:02 +0530

    Create SpringBoot App

---
+++ src/test/java/com/shoryakaushik/urlShortnerBackend/UrlShortnerBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shoryakaushik.urlShortnerBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UrlShortnerBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
